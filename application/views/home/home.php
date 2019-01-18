40e98189f3f039c9fbbed31bab2c4a5675eba0df
mochammad <mochammadfadhlizhil@gmail.com>
2019-01-18 10:46:43 +0700

    Scrollingspy

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -1,17 +1,15 @@
 <!-- Navbar -->
-<nav class="navbar navbar-expand-lg navbar-light">
+<nav id="navbar-example2" class="navbar navbar-expand-lg navbar-light">
   <div class="container">
-
     <a class="navbar-brand" href="#">Bipa.id</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav ml-auto">
-        <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-        <a class="nav-item nav-link" href="#">Your Benefit</a>
-        <a class="nav-item nav-link" href="#">Our Programs</a>
-        <a class="nav-item nav-link" href="#">Our Tutors</a>
+        <a class="nav-item nav-link" href="#yourbenefit">Your Benefit</a>
+        <a class="nav-item nav-link" href="#ourprograms">Our Programs</a>
+        <a class="nav-item nav-link" href="#ourtutors">Our Tutors</a>
         <a class="nav-item btn btn-secondary tombol" href="#" data-toggle="modal" data-target="#exampleModalCenter">Login</a>
         <!-- Modal -->
         <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
@@ -55,205 +53,199 @@
 </div>
 <!-- Akhir Jumbotron -->
 
+<div data-spy="scroll" data-target="#navbar-example2" data-offset="0">
 <!-- Your Benefit -->
-<section class="yourbenefit">
-  <div class="container">
+  <section class="yourbenefit" id="yourbenefit">
+    <div class="container">
+      <h3 class="text-center">Your Benefits</h3>
+      <div class="row justify-content-center">
+        <div class="col justify-content-center d-flex">
+          <figure class="figure">
+            <img src="<?=base_url()?>assets/images/avatar-blank.jpg" class="figure-img img-fluid rounded-circle" alt="">
+            <figcaption class="figure-caption">
+              <h5 class="text-center">Manage Schedule by Yourself</h5>
+            </figcaption>
+          </figure>
+        </div>
 
-    <h3 class="text-center">Your Benefits</h3>
-    <div class="row justify-content-center">
-      <div class="col justify-content-center d-flex">
-        <figure class="figure">
-          <img src="<?=base_url()?>assets/images/avatar-blank.jpg" class="figure-img img-fluid rounded-circle" alt="">
-          <figcaption class="figure-caption">
-            <h5 class="text-center">Manage Schedule by Yourself</h5>
-          </figcaption>
-        </figure>
-      </div>
+        <div class="col justify-content-center d-flex">
+          <figure class="figure">
+            <img src="<?=base_url()?>assets/images/avatar-blank.jpg" class="figure-img img-fluid rounded-circle" alt="">
+            <figcaption class="figure-caption">
+              <h5 class="text-center">Decide your goal lesson</h5>
+            </figcaption>
+          </figure>
+        </div>
 
-      <div class="col justify-content-center d-flex">
-        <figure class="figure">
-          <img src="<?=base_url()?>assets/images/avatar-blank.jpg" class="figure-img img-fluid rounded-circle" alt="">
-          <figcaption class="figure-caption">
-            <h5 class="text-center">Decide your goal lesson</h5>
-          </figcaption>
-        </figure>
+        <div class="col justify-content-center d-flex">
+          <figure class="figure">
+            <img src="<?=base_url()?>assets/images/avatar-blank.jpg" class="figure-img img-fluid rounded-circle" alt="">
+            <figcaption class="figure-caption">
+              <h5 class="text-center">Connecting with native speaker tutors</h5>
+            </figcaption>
+          </figure>
+        </div>
+        
+        <div class="col justify-content-center d-flex">
+          <figure class="figure">
+            <img src="<?=base_url()?>assets/images/avatar-blank.jpg" class="figure-img img-fluid rounded-circle" alt="">
+            <figcaption class="figure-caption">
+              <h5 class="text-center">Tested method</h5>
+            </figcaption>
+          </figure>
+        </div>
       </div>
-
-      <div class="col justify-content-center d-flex">
-        <figure class="figure">
-          <img src="<?=base_url()?>assets/images/avatar-blank.jpg" class="figure-img img-fluid rounded-circle" alt="">
-          <figcaption class="figure-caption">
-            <h5 class="text-center">Connecting with native speaker tutors</h5>
-          </figcaption>
-        </figure>
-      </div>
-      
-      <div class="col justify-content-center d-flex">
-        <figure class="figure">
-          <img src="<?=base_url()?>assets/images/avatar-blank.jpg" class="figure-img img-fluid rounded-circle" alt="">
-          <figcaption class="figure-caption">
-            <h5 class="text-center">Tested method</h5>
-          </figcaption>
-        </figure>
+      <div class="row justify-content-center">
+        <a class="nav-item btn btn-dark tombol" href="#" data-toggle="modal" data-target="#exampleModalCenter">Book a class</a>
       </div>
     </div>
-    <div class="row justify-content-center">
-      <a class="nav-item btn btn-dark tombol" href="#" data-toggle="modal" data-target="#exampleModalCenter">Book a class</a>
-    </div>
-  </div>
-</section>
+  </section>
 <!-- Akhir your benefit -->
 
 <!-- Our Program -->
-<section class="ourprograms">
-  <div class="container">
-    <br>
-    <h3 class="text-center">Our Programs</h3>
-    <div class="row justify-content-center">
-      <div class="col justify-content-center d-flex">
-        <figure class="figure">
-          <h4>1st Program</h4>
-          <img src="<?=base_url()?>assets/images/avatar-blank.jpg" class="figure-img img-fluid rounded" alt="">
-          <figcaption class="figure-caption">
-            <h5 class="text-center">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries
-            </h5>
-          </figcaption>
-        </figure>
-      </div>
+  <section class="ourprograms" id="ourprograms">
+    <div class="container">
+      <br>
+      <h3 class="text-center">Our Programs</h3>
+      <div class="row justify-content-center">
+        <div class="col justify-content-center d-flex">
+          <figure class="figure">
+            <h4>1st Program</h4>
+            <img src="<?=base_url()?>assets/images/avatar-blank.jpg" class="figure-img img-fluid rounded" alt="">
+            <figcaption class="figure-caption">
+              <h5 class="text-center">
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries
+              </h5>
+            </figcaption>
+          </figure>
+        </div>
 
-      <div class="col justify-content-center d-flex">
-        <figure class="figure">
-          <h4>2st Program</h4>
-          <img src="<?=base_url()?>assets/images/avatar-blank.jpg" class="figure-img img-fluid rounded" alt="">
-          <figcaption class="figure-caption">
-            <h5 class="text-center">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries
-            </h5>
-          </figcaption>
-        </figure>
-      </div>
+        <div class="col justify-content-center d-flex">
+          <figure class="figure">
+            <h4>2st Program</h4>
+            <img src="<?=base_url()?>assets/images/avatar-blank.jpg" class="figure-img img-fluid rounded" alt="">
+            <figcaption class="figure-caption">
+              <h5 class="text-center">
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries
+              </h5>
+            </figcaption>
+          </figure>
+        </div>
 
-      <div class="col justify-content-center d-flex">
-        <figure class="figure">
-          <h4>3st Program</h4>
-          <img src="<?=base_url()?>assets/images/avatar-blank.jpg" class="figure-img img-fluid rounded" alt="">
-          <figcaption class="figure-caption">
-            <h5 class="text-center">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries
-            </h5>
-          </figcaption>
-        </figure>
+        <div class="col justify-content-center d-flex">
+          <figure class="figure">
+            <h4>3st Program</h4>
+            <img src="<?=base_url()?>assets/images/avatar-blank.jpg" class="figure-img img-fluid rounded" alt="">
+            <figcaption class="figure-caption">
+              <h5 class="text-center">
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries
+              </h5>
+            </figcaption>
+          </figure>
+        </div>
       </div>
+      <div class="row justify-content-center">
+        <a class="nav-item btn btn-dark tombol" href="#" data-toggle="modal" data-target="#exampleModalCenter">Learn More</a>
+      </div>
+      <br>
     </div>
-    <div class="row justify-content-center">
-      <a class="nav-item btn btn-dark tombol" href="#" data-toggle="modal" data-target="#exampleModalCenter">Learn More</a>
-    </div>
-    <br>
-  </div>
-</section>
+  </section>
 <!-- Akhir Our Program -->
 
 <!-- Out tutors -->
-<section class="ourtutors">
-  <div class="container">
-  <br>
-  <h3 class="text-center">Our Tutors</h3>
-    <div class="row">
-      <div class="col">
-        <div class="card">
-          <figure class="figure">
-            <img src="<?=base_url()?>assets/images/avatar-blank.jpg" class="figure-img img-fluid rounded-circle" alt="">
-          </figure>
-          <div class="card-body">
-            <h5 class="card-title">Anwar Ali</h5>
+  <section class="ourtutors" id="ourtutors">
+    <div class="container">
+    <br>
+    <h3 class="text-center">Our Tutors</h3>
+      <div class="row">
+        <div class="col">
+          <div class="card">
             <figure class="figure">
-            <img src="<?=base_url()?>assets/images/rating-black.png" class="figure-img img-fluid" alt="" style="width:110px">
+              <img src="<?=base_url()?>assets/images/avatar-blank.jpg" class="figure-img img-fluid rounded-circle" alt="">
             </figure>
-            <div class="row justify-content-center">
-              <div class="col d-flex justify-content-center">
-                <a href="#" class="btn btn-dark">View Profil</a>
+            <div class="card-body">
+              <h5 class="card-title">Anwar Ali</h5>
+              <figure class="figure">
+              <img src="<?=base_url()?>assets/images/rating-black.png" class="figure-img img-fluid" alt="" style="width:110px">
+              </figure>
+              <div class="row justify-content-center">
+                <div class="col d-flex justify-content-center">
+                  <a href="#" class="btn btn-dark">View Profil</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card">
+            <figure class="figure">
+              <img src="<?=base_url()?>assets/images/avatar-blank.jpg" class="figure-img img-fluid rounded-circle" alt="">
+            </figure>
+            <div class="card-body">
+              <h5 class="card-title">Anwar Ali</h5>
+              <figure class="figure">
+              <img src="<?=base_url()?>assets/images/rating-black.png" class="figure-img img-fluid" alt="" style="width:110px">
+              </figure>
+              <div class="row justify-content-center">
+                <div class="col d-flex justify-content-center">
+                  <a href="#" class="btn btn-dark">View Profil</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card">
+            <figure class="figure">
+              <img src="<?=base_url()?>assets/images/avatar-blank.jpg" class="figure-img img-fluid rounded-circle" alt="">
+            </figure>
+            <div class="card-body">
+              <h5 class="card-title">Anwar Ali</h5>
+              <figure class="figure">
+              <img src="<?=base_url()?>assets/images/rating-black.png" class="figure-img img-fluid" alt="" style="width:110px">
+              </figure>
+              <div class="row justify-content-center">
+                <div class="col d-flex justify-content-center">
+                  <a href="#" class="btn btn-dark">View Profil</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card">
+            <figure class="figure">
+              <img src="<?=base_url()?>assets/images/avatar-blank.jpg" class="figure-img img-fluid rounded-circle" alt="">
+            </figure>
+            <div class="card-body">
+              <h5 class="card-title">Anwar Ali</h5>
+              <figure class="figure">
+              <img src="<?=base_url()?>assets/images/rating-black.png" class="figure-img img-fluid" alt="" style="width:110px">
+              </figure>
+              <div class="row justify-content-center">
+                <div class="col d-flex justify-content-center">
+                  <a href="#" class="btn btn-dark">View Profil</a>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-
-      <div class="col">
-        <div class="card">
-          <figure class="figure">
-            <img src="<?=base_url()?>assets/images/avatar-blank.jpg" class="figure-img img-fluid rounded-circle" alt="">
-          </figure>
-          <div class="card-body">
-            <h5 class="card-title">Anwar Ali</h5>
-            <figure class="figure">
-            <img src="<?=base_url()?>assets/images/rating-black.png" class="figure-img img-fluid" alt="" style="width:110px">
-            </figure>
-            <div class="row justify-content-center">
-              <div class="col d-flex justify-content-center">
-                <a href="#" class="btn btn-dark">View Profil</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="col">
-        <div class="card">
-          <figure class="figure">
-            <img src="<?=base_url()?>assets/images/avatar-blank.jpg" class="figure-img img-fluid rounded-circle" alt="">
-          </figure>
-          <div class="card-body">
-            <h5 class="card-title">Anwar Ali</h5>
-            <figure class="figure">
-            <img src="<?=base_url()?>assets/images/rating-black.png" class="figure-img img-fluid" alt="" style="width:110px">
-            </figure>
-            <div class="row justify-content-center">
-              <div class="col d-flex justify-content-center">
-                <a href="#" class="btn btn-dark">View Profil</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="col">
-        <div class="card">
-          <figure class="figure">
-            <img src="<?=base_url()?>assets/images/avatar-blank.jpg" class="figure-img img-fluid rounded-circle" alt="">
-          </figure>
-          <div class="card-body">
-            <h5 class="card-title">Anwar Ali</h5>
-            <figure class="figure">
-            <img src="<?=base_url()?>assets/images/rating-black.png" class="figure-img img-fluid" alt="" style="width:110px">
-            </figure>
-            <div class="row justify-content-center">
-              <div class="col d-flex justify-content-center">
-                <a href="#" class="btn btn-dark">View Profil</a>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div class="row justify-content-center">
+        <button class="nav-item btn btn-dark tombol" href="#" data-toggle="modal" data-target="#exampleModalCenter">View All Tutors</button>
       </div>
     </div>
-    <div class="row justify-content-center">
-      <button class="nav-item btn btn-dark tombol" href="#" data-toggle="modal" data-target="#exampleModalCenter">View All Tutors</button>
-    </div>
-  </div>
-</section>
-
-
-
-
+  </section>
 <!-- Akhir our tutors -->
-
+</div>
 
 <!-- footer -->
 <section class="footer">
   <br>
   <div class="container">
-
-    
     <div class="row">
       <div class="col ml-5">
         <h4>Contact Us</h4>
@@ -279,8 +271,7 @@
         <h3>laboramus est, ne eum</h3>
         <h3>malis</h3>
       </div>
-    </div>
-    
+    </div>   
   </div>
 </section>
 <!-- Akhir footer -->
